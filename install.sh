ea73f7e89c42ab051ae797ef06696446bc581049
mr_artan <44286111+msaebi031@users.noreply.github.com>
2025-03-03 16:46:20 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,18 @@ FILE_NAME="backhaul_${OS}_${ARCH}.tar.gz"
 
 echo "Downloading $FILE_NAME..."
 curl -L -O "https://github.com/Musixal/Backhaul/releases/latest/download/$FILE_NAME"
+if [ $? -ne 0 ]; then
+    echo "Download failed!"
+    exit 1
+fi
 
-mkdir -p /root/backhaul && tar -xzf "$FILE_NAME" -C /root/backhaul && \
-rm -f "$FILE_NAME" /root/backhaul/LICENSE /root/backhaul/README.md && \
-echo "Extraction successful" || {
+mkdir -p /root/backhaul && tar -xzf "$FILE_NAME" -C /root/backhaul
+if [ $? -ne 0 ]; then
     echo "Extraction failed!"
     exit 1
-}
+fi
+
+rm -f "$FILE_NAME" /root/backhaul/LICENSE /root/backhaul/README.md
+echo "Extraction successful, cleaned up files."
 
 cd /root/backhaul/
